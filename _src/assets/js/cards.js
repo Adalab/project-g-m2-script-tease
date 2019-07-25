772dda254e16c5f1356944a76899cf26eb141890
pascualbeatriz <pascuals.beatriz@gmail.com>
2019-07-25 15:50:17 +0200

    Fix phone input in js

--- _src/assets/js/cards.js
+++ _src/assets/js/cards.js
@@ -5,6 +5,7 @@
 const inputName = document.querySelector('.name');
 const inputJob = document.querySelector('.puesto');
 const inputMail = document.querySelector('.mail');
+const inputPhone = document.querySelector('.phone');
 const inputLinkedin = document.querySelector('.linkedin_input');
 const inputGithub = document.querySelector('.github_input');
 
@@ -13,6 +14,7 @@ const inputGithub = document.querySelector('.github_input');
 const textName = document.querySelector('.img-text__title');
 const textJob = document.querySelector('.img-text__subtitle');
 const textMail = document.querySelector('.icon__mail');
+const textPhone = document.querySelector('.icon__mobile');
 const textLinkedin = document.querySelector('.icon__linkedin');
 const textGithub = document.querySelector('.icon__github');
 
@@ -25,6 +27,14 @@ function write(event){
   else if(event.currentTarget.classList.contains('puesto')){
     textJob.innerHTML = inputValue;
   }
+  else if(event.currentTarget.classList.contains('phone')){
+    textPhone.href = 'phoneto:' + inputValue;
+    if(textPhone.classList.contains('hidden_v')){
+      textPhone.classList.remove('hidden_v');
+    }else if(textPhone.href === 'phoneto:'){
+      textPhone.classList.add('hidden_v');
+    }
+  }
   else if(event.currentTarget.classList.contains('mail')){
     textMail.href = 'mailto:' + inputValue;
     if(textMail.classList.contains('hidden_v')){
@@ -32,19 +42,22 @@ function write(event){
     }else if(textMail.href === 'mailto:'){
       textMail.classList.add('hidden_v');
     }
-
   }
   else if(event.currentTarget.classList.contains('linkedin_input')){
     textLinkedin.href = inputValue;
     if(textLinkedin.classList.contains('hidden_v')){
       textLinkedin.classList.remove('hidden_v');
-    }else if(textLinkedin.href === ''){
+    }else if(inputValue === ""){
       textLinkedin.classList.add('hidden_v');
     }
   }
   else if(event.currentTarget.classList.contains('github_input')){
     textGithub.href = inputValue;
-    textGithub.classList.toggle('hidden_v');
+    if(textGithub.classList.contains('hidden_v')){
+      textGithub.classList.remove('hidden_v');
+    }else if(inputValue === ""){
+      textGithub.classList.add('hidden_v');
+    }
   }
   console.log(event.currentTarget);
 }
@@ -52,5 +65,6 @@ function write(event){
 inputName.addEventListener('keyup', write);
 inputJob.addEventListener('keyup', write);
 inputMail.addEventListener('keyup', write);
+inputPhone.addEventListener('keyup', write);
 inputLinkedin.addEventListener('keyup', write);
 inputGithub.addEventListener('keyup', write);
