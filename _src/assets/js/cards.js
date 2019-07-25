7a4049d618a99d51ad6257a3177c07d5dbbce13b
Migara <evamarcod@gmail.com>
2019-07-25 14:27:44 +0200

    Add hidden class to mail

--- _src/assets/js/cards.js
+++ _src/assets/js/cards.js
@@ -6,6 +6,7 @@ const inputName = document.querySelector('.name');
 const inputJob = document.querySelector('.puesto');
 const inputMail = document.querySelector('.mail');
 const inputLinkedin = document.querySelector('.linkedin_input');
+const inputGithub = document.querySelector('.github_input');
 
 /*Variables que almacenan los destinos (donde se va a ver)*/
 
@@ -13,6 +14,7 @@ const textName = document.querySelector('.img-text__title');
 const textJob = document.querySelector('.img-text__subtitle');
 const textMail = document.querySelector('.icon__mail');
 const textLinkedin = document.querySelector('.icon__linkedin');
+const textGithub = document.querySelector('.icon__github');
 
 function write(event){
   let inputValue = event.currentTarget.value;
@@ -25,9 +27,24 @@ function write(event){
   }
   else if(event.currentTarget.classList.contains('mail')){
     textMail.href = 'mailto:' + inputValue;
+    if(textMail.classList.contains('hidden_v')){
+      textMail.classList.remove('hidden_v');
+    }else if(textMail.href === 'mailto:'){
+      textMail.classList.add('hidden_v');
+    }
+
   }
   else if(event.currentTarget.classList.contains('linkedin_input')){
     textLinkedin.href = inputValue;
+    if(textLinkedin.classList.contains('hidden_v')){
+      textLinkedin.classList.remove('hidden_v');
+    }else if(textLinkedin.href === ''){
+      textLinkedin.classList.add('hidden_v');
+    }
+  }
+  else if(event.currentTarget.classList.contains('github_input')){
+    textGithub.href = inputValue;
+    textGithub.classList.toggle('hidden_v');
   }
   console.log(event.currentTarget);
 }
@@ -36,3 +53,4 @@ inputName.addEventListener('keyup', write);
 inputJob.addEventListener('keyup', write);
 inputMail.addEventListener('keyup', write);
 inputLinkedin.addEventListener('keyup', write);
+inputGithub.addEventListener('keyup', write);
