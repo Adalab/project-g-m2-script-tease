8c643f370376f6d065b7a818533ac4d044459321
Migara <evamarcod@gmail.com>
2019-07-25 14:03:34 +0200

    Add listeners to job, linkedin and mail

--- _src/assets/js/cards.js
+++ _src/assets/js/cards.js
@@ -1,73 +1,38 @@
 'use strict';
 
-// const inputText = document.querySelector('.form__fieldset-content-gap');
-// const textName = document.querySelector('.img-text__title');
-// const textJob = document.querySelector('.img-text__subtitle');
-
-
-// function writeInstant(event) {
-//   console.log(event.currentTarget);
-//   let inputValueText = event.target.value;
-
-//   if(event.target.classList.contains('name')){
-//     textName.innerHTML = inputValueText
-//   }else if(event.target.classList.contains('puesto')){
-//     textJob.innerHTML = inputValueText;
-//     console.log('Esto es el puesto?')
-//     console.log(event.target);
-//     }
-// }
-
-// inputText.addEventListener('keyup', writeInstant);
-
-// const form = document.querySelector('.form__all-sections');
-// const inputName = document.querySelector('.name');
-// const inputJon = document.querySelector('.puesto');
-// const inputText = document.querySelector('.form__fieldset-content-gap');
-// const textName = document.querySelector('.img-text__title');
-// const textJob = document.querySelector('.img-text__subtitle');
-
-
-// function writeInstant(event) {
-//   console.log(event.currentTarget);
-//     let inputValueText = event.currentTarget.value;
-//      if(event.currentTarget.classList.contains('name')){
-//        textName.innerHTML = inputValueText
-//        console.log(event.currentTarget);
-//      }else if(event.currentTarget.classList.contains('puesto')){
-//        textJob.innerHTML = inputValueText;
-//        console.log('Esto es el puesto?')
-//        console.log(event.target);
-//      }
-// }
-
-// form.addEventListener('keyup', writeInstant);
+/* Variables que almacenan los input*/
 
 const inputName = document.querySelector('.name');
 const inputJob = document.querySelector('.puesto');
+const inputMail = document.querySelector('.mail');
+const inputLinkedin = document.querySelector('.linkedin_input');
+
+/*Variables que almacenan los destinos (donde se va a ver)*/
+
 const textName = document.querySelector('.img-text__title');
 const textJob = document.querySelector('.img-text__subtitle');
+const textMail = document.querySelector('.icon__mail');
+const textLinkedin = document.querySelector('.icon__linkedin');
 
 function write(event){
   let inputValue = event.currentTarget.value;
+
   if(event.currentTarget.classList.contains('name')){
     textName.innerHTML = inputValue;
   }
   else if(event.currentTarget.classList.contains('puesto')){
     textJob.innerHTML = inputValue;
   }
+  else if(event.currentTarget.classList.contains('mail')){
+    textMail.href = 'mailto:' + inputValue;
+  }
+  else if(event.currentTarget.classList.contains('linkedin_input')){
+    textLinkedin.href = inputValue;
+  }
   console.log(event.currentTarget);
 }
 
-
-
-// function write2(event){
-//   let inputValue = event.currentTarget.value;
-//   textJob.innerHTML = inputValue;
-// }
-
-
-
-
 inputName.addEventListener('keyup', write);
 inputJob.addEventListener('keyup', write);
+inputMail.addEventListener('keyup', write);
+inputLinkedin.addEventListener('keyup', write);
