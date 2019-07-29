75acd777a0f20eba7cd5adb3d36f175174d5ff9f
silvia <silviademiguel7@gmail.com>
2019-07-29 10:50:23 +0200

    fix btn share center

--- _src/assets/js/desplegables.js
+++ _src/assets/js/desplegables.js
@@ -1,21 +1,21 @@
 'use strict';
 const containerClick = document.querySelectorAll('.containerClick-js');
-console.log(containerClick);
+//console.log(containerClick);
 const containerColorsRoll = document.querySelectorAll('.rollContainer-js');
-console.log(containerColorsRoll);
+//console.log(containerColorsRoll);
 const containerMother = document.querySelectorAll('.containerMother-js');
-console.log(containerMother);
+//console.log(containerMother);
 
 function rollup (event){
-  console.log('elementoclick',event.currentTarget);
-  console.log('id',event.currentTarget.id);
-  console.log('padre',event.currentTarget.parentNode)
+  //console.log('elementoclick',event.currentTarget);
+  //console.log('id',event.currentTarget.id);
+  //console.log('padre',event.currentTarget.parentNode)
   event.currentTarget.parentNode.classList.toggle('open');
 }
 for(let i = 0; i < containerClick.length; i++){
   containerClick[i].addEventListener('click',rollup);
 
 }
-console.log(containerClick);
+//console.log(containerClick);
 //containerClick.addEventListener('click',rollup);
 
