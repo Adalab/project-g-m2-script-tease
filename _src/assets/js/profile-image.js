78f8650bde94688d8d05dd0f275e9b232342f59c
Alba Gómez Pérez <albagope@gmail.com>
2019-08-01 08:38:59 +0200

    upload image javaScript

--- _src/assets/js/profile-image.js
+++ _src/assets/js/profile-image.js
@@ -1,25 +1,31 @@
 'use strict';
 
-const fr = new FileReader();
-const uploadBtn = document.querySelector('.js__profile-trigger');
-const fileField = document.querySelector('.js__profile-upload-btn');
-const profileImage = document.querySelector('.js__profile-image');
-const profilePreview = document.querySelector('.js__profile-preview');
+// fileReader es para leer datos
+const read = new FileReader();
+const uploadButton = document.querySelector('.js__profile-trigger');
+const input = document.querySelector('.js__profile-upload-btn');
+const imageContain = document.querySelector('.js__profile-image');
+const imagePreview = document.querySelector('.js__profile-preview');
 
-function getImage(e){
+// function getImage(e){
+function selectorImage(e) {
   var myFile = e.currentTarget.files[0];
-  fr.addEventListener('load', writeImage);
-  fr.readAsDataURL(myFile);
+  read.addEventListener('load', writeImage);
+  read.readAsDataURL(myFile);
 }
 
 function writeImage() {
-  profileImage.style.backgroundImage = `url(${fr.result})`;
-  profilePreview.style.backgroundImage = `url(${fr.result})`;
+  imageContain.style.backgroundImage = `url(${read.result})`;
+  imagePreview.style.backgroundImage = `url(${read.result})`;
 }
 
-function fakeFileClick() {
- fileField.click();
+// function fakeFileClick() {
+function selectorImage() {
+input.click();
 }
 
-uploadBtn.addEventListener('click', fakeFileClick);
-fileField.addEventListener('change', getImage);
+// Evento click sobre el botón "añadir imagen"
+uploadButton.addEventListener('click', addImage);
+
+// Evento cambiar sobre la parte hidden (input)
+input.addEventListener('change', selectorImage);
