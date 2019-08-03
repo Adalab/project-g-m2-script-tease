7c7c8b3d311c106878801c39154443bb2ab9ad92
Alba Gómez Pérez <albagope@gmail.com>
2019-08-03 12:08:36 +0200

    Show section twitter when is properly

--- _src/assets/js/desplegables.js
+++ _src/assets/js/desplegables.js
@@ -6,6 +6,10 @@ const containerColorsRoll = document.querySelectorAll('.rollContainer-js');
 const containerMother = document.querySelectorAll('.containerMother-js');
 //console.log(containerMother);
 
+const containerTwitter = document.querySelector('.hide-twitter');
+const createButton = document.querySelector('.share-button-create');
+
+
 function rollup (event){
   //console.log('elementoclick',event.currentTarget);
   //console.log('id',event.currentTarget.id);
@@ -14,11 +18,10 @@ function rollup (event){
   if (event.currentTarget.parentElement.classList.contains('open')){
     event.currentTarget.parentElement.classList.remove('open');
   }else{
-   for(const item of containerColorsRoll){
-     item.parentElement.classList.remove('open');
-   }
+    for(const item of containerColorsRoll){
+      item.parentElement.classList.remove('open');
+    }
     event.currentTarget.parentElement.classList.add('open');
-
   }
 
 }
@@ -26,6 +29,13 @@ for(let i = 0; i < containerClick.length; i++){
   containerClick[i].addEventListener('click',rollup);
 
 }
+
 //console.log(containerClick);
 //containerClick.addEventListener('click',rollup);
+
+function showTwitterSection(){
+  containerTwitter.classList.remove('hide-twitter');
+}
+
+createButton.addEventListener('click', showTwitterSection);
 
